52100c69497ba0943410b7e3752175c0781208ef
cc_JIA <64264014+ccjia39@users.noreply.github.com>
2025-11-05 17:50:53 +0800

    SCASCC_codes_one_ele

---
+++ SCASCC/RunExample.sh
@@ -0,0 +1,16 @@
+#source /public/intel/oneapi/setvars.sh intel64
+#FLIB_ROOT=/storage/home/hznu/jiacc/xmlf90/macros ; export FLIB_ROOT
+dir=$(pwd)
+direx=$(pwd)'/Example/'
+echo $dir
+echo $direx
+
+cd $direx
+sed "s#dir#$dir#g" getsta_h.tmp > getsta_h.xml
+$dir/bin/GetSta getsta_h
+sed "s#dir#$dir#g" getsta_nitrogen.tmp > getsta_nitrogen.xml
+$dir/bin/GetSta getsta_nitrogen
+$dir/bin/Coll collinput
+$dir/bin/Sigma
+cd ..
+
